0567239bcf51a7a1bcce6c5cfed9659f21536e10
rsmit724 <138529566+rsmit724@users.noreply.github.com>
2023-09-15 13:26:45 -0700

    added error message for input field when left blank

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,12 @@ function newItem() {
     let inputValue = $('#input').val();
     li.append(inputValue);
 
+    if (inputValue === '') {
+        alert('You must write something!');
+    } else {
+        $('#list').append(li);
+    }
+
 
 }
 
